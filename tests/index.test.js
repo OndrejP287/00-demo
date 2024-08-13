4850e5cee8461105c6fda8c4d59a7c18c3a866fc
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-08-13 12:35:13 +0000

    Added some tests.

--- tests/index.test.js
+++ tests/index.test.js
@@ -13,8 +13,18 @@ describe('index.html', () => {
         expect(elem).toBeDefined()
     })
 
-    test("Das <h1>-Tag hat den Inhalt 'Hello World'", () => {
+    test('Das <h1>-Tag hat einen Namen statt ...', () => {
         const elem = body.querySelector('h1')
-        expect(elem.textContent).toBe('Hello World!')
+        expect(elem.textContent).not.toMatch('...')
+    })
+
+    test('Die Webseite hat ein <section>-Tag.', () => {
+        const elem = body.querySelector('section')
+        expect(elem).toBeDefined()
+    })
+
+    test('Das <section>-Tag ist innerhalb des <main>-Tag.', () => {
+        const elem = body.querySelector('main>section')
+        expect(elem).not.toBeNull()
     })
 })
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import FullReload from 'vite-plugin-full-reload'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-    plugins: [react(), svgr(), FullReload(['images/**/*.svg'])],
+    plugins: [react(), FullReload(['index.html'])],
     server: {
         host: '0.0.0.0'
     },
@@ -17,5 +16,5 @@ export default defineConfig({
         testIdAttribute: 'id'
     },
 
-    base: '/01-intro-websites-cedricgeissmann/'
+    base: '/00-demo/'
 })
